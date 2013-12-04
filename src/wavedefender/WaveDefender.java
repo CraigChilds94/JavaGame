5f5c4af1eac7a770956586975c65829e478f4935
Bertdasquirt <childscraig17@gmail.com>
2013-12-04 08:21:23 +0000

    Hopefully it'll show the right username now :(

--- src/wavedefender/WaveDefender.java
+++ src/wavedefender/WaveDefender.java
@@ -31,7 +31,7 @@ public class WaveDefender extends BasicGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        // Meh
         try {
             AppGameContainer appgc;
             appgc = new AppGameContainer(new WaveDefender("WaveDefender - " + WaveDefender.VERSION));
