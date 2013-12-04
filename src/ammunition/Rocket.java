b843c125f975e84e0181dd41a026cc6dff002e1e
craigchilds <childscraig17@gmail.com>
2013-12-04 08:12:21 +0000

    Added lib to make things easier

--- src/ammunition/Rocket.java
+++ src/ammunition/Rocket.java
@@ -3,10 +3,7 @@ package ammunition;
 import gameobjects.Ammunition;
 import gameobjects.Collidable;
 
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 /**
